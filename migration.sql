30b7fd9b01110755ad1eeeb8d64fe370d8b21e70
alifradz <149651000+alifradz@users.noreply.github.com>
2023-12-05 16:57:25 +0700

    Create migration.sql

---
+++ migration.sql
@@ -0,0 +1,11 @@
+-- prisma/migrations/init_menu_table.sql
+
+-- Migration `init_menu_table`
+
+CREATE TABLE "Menu" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(255) NOT NULL,
+  "price" INT NOT NULL,
+  "createdAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "updatedAt" TIMESTAMP NOT NULL
+);
